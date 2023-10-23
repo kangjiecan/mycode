5c98ef03ef7d75d4b43fbbce07ef1583aed9dad0
kangjiecan <kangjiecan@gmail.com>
2023-10-23 11:00:08 -0300

    update

--- python/leetcode/longestCommonPrefix.py
+++ python/leetcode/longestCommonPrefix.py
@@ -28,24 +28,25 @@ strs[i] consists of only lowercase English letters."""
 
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-     i=0
      p=""
      k=len(strs)
-     index=0
+     j=0
      strs=sorted(strs,key=len)
-     print(strs)
      if len(strs)==1:
         return strs[0]
      if not strs:
             return ""
-     for index in range(len(strs[i])): 
-        char=strs[0][index]  
-        for i in strs:        
-         if strs[index]!=char or index==len(strs[i])-1:
-            return p
-         elif strs[index]==char and i+1==k-1:
-            p +=char
+     for index in range(len(strs[0])): 
+        char=strs[0][index] 
+        for i in strs: 
+         j=j+1
+         if i[index]!=char :
+            return p   
+         elif j==len(strs):
+            p+=char
+            j=0
      return p
+  
 tryout=["flower","flow","flight"]
 tryout1=Solution()
 result=tryout1.longestCommonPrefix(tryout)
