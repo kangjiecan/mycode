e33c225c4764022ba7d00eec12005f3c70289948
kangjiecan <kangjiecan@gmail.com>
2023-10-24 13:12:14 -0300

    update

--- python/leetcode/longest_substring_without_repeating_1.py
+++ python/leetcode/longest_substring_without_repeating_1.py
@@ -27,39 +27,35 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 """
-#improve brute force by using a hash table stores the duplicate letter index. it reduces a track back loop compare to previous version
+
+# improve brute force by using a hash table stores the duplicate letter index. 
+# it reduces a track back loop for next search begining index compare to previous version
+# push s[count] and its index to stack if it is not in stack.
+# find the duplicated s[count] index in the stack, use thid index +1 as next search beginning.
+# output the max length value
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         stack={}
         count=0
         length=0
+        
+        # find the duplicated s[count] index in the stack, use thid index +1 as next search beginning.
 
         while count<len(s):
          if s[count] in stack:  
-           if len(stack) > length:
-            length=len(stack)
+          
            count = stack[s[count]]+1
            stack = {}
-
+           
+         #  push s[count] and its index to stack if it is not in stack.
          if s[count] not in stack:
              stack[s[count]]=count
                               
          count +=1
 
-        if len(stack)>length:
-           return len(stack)
-        else:
-           return length
-
-    
-       
-                 
-
-
-        
-
+         length=max(length,len(stack))
+        return length 
+      
 test=Solution()
 result=test.lengthOfLongestSubstring("abcdef")   
 print(result)             
-
-
--- python/leetcode/scatch.py
+++ python/leetcode/scatch.py
@@ -1,22 +1,2 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        stack={}
-        count=0
-        length=0
 
-        while count<len(s):
-         if s[count] in stack:  
-           if len(stack) > length:
-            length=len(stack)
-           count = stack[s[count]]+1
-           stack = {}
-
-         if s[count] not in stack:
-             stack[s[count]]=count
-                              
-         count +=1
-
-        if len(stack)>length:
-           return len(stack)
-        else:
-           return length
+          
