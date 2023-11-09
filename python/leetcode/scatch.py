01ad5367b7c66708e03ba14501a475ba312d4012
kangjiecan <129395660+kangjiecan@users.noreply.github.com>
2023-11-09 11:29:56 -0400

    upload

--- python/leetcode/scatch.py
+++ python/leetcode/scatch.py
@@ -1,8 +1,26 @@
-a={}
-b=1
-c=2
-a[1]=a.get(1,0)+1
-a[2]=a.get(2,0)+1
-a[1]=a.get(1,0)+1
-a[1]=a[1]-1
-print(a)
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        hashmaping=[]
+        left=0
+        right=2
+        count=0
+        while right<=len(s)-1:
+            for i in range(left,right+1):
+                if s[i] in hashmaping:
+                  break
+                if s[i] not in hashmaping:
+                    hashmaping.append(s[i])  
+            if len(hashmaping)==3:
+                count +=1
+            hashmaping=[] 
+            right+=1
+            left +=1   
+        return count
+
+
+
+
+s = "xyzzaz"
+run=Solution()              
+result=run.countGoodSubstrings(s)
+print(result)
--- python/school_assessment/skatch.py
+++ python/school_assessment/skatch.py
@@ -1,19 +1,32 @@
-
 class Solution:
-    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-      left = 0
-      right = 0
-      temp = 0
-      length = 0
-      char = ""
-      
-      
-      
-      while right < len(s):
-        char = set(s[left:right + 1])
-        if len(char) <= 2:
-         right += 1
-         temp = right - left
-         length = max(length, temp)
-        else: left += 1
-      return length
+    def countGoodSubstrings(self, s: str) -> int:
+        if len(s) < 3:
+            return 0  # Early return if it's impossible to have a good substring
+        
+        count = 0
+        # Initialize the set with the first 3 characters if possible
+        char_set = set(s[:3])
+        
+        # If the first 3 characters are unique, count it as a good substring
+        if len(char_set) == 3:
+            count += 1
+        
+        for i in range(3, len(s)):
+            # Remove the leftmost character
+            char_set.discard(s[i-3])
+            # Add the new character
+            char_set.add(s[i])
+            # If there are 3 unique characters, it's a good substring
+            if len(char_set) == 3:
+                count += 1
+            else:
+                # This is necessary to ensure that the set size does not exceed 3
+                char_set.discard(s[i])
+        
+        return count
+
+
+s = "aababcabc"
+run=Solution()              
+result=run.countGoodSubstrings(s)
+print(result)
