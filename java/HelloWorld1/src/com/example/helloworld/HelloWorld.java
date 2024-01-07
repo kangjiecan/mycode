0095474c7e082196d48229d93bbf4b784c3f905a
kangjiecan <kangjiecan@gmail.com>
2024-01-07 13:55:53 -0400

    all

--- java/HelloWorld1/src/com/example/helloworld/HelloWorld.java
+++ java/HelloWorld1/src/com/example/helloworld/HelloWorld.java
@@ -1,2 +1,8 @@
-package com.example.helloworld;public class HelloWorld {
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
 }
