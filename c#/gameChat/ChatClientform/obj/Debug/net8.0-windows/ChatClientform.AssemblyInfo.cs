31317e7dc2585507d7f4ea8af3244479e28c1793
kangjiecan <kangjiecan@gmail.com>
2024-10-12 12:17:02 -0400

    update

--- c#/gameChat/ChatClientform/obj/Debug/net8.0-windows/ChatClientform.AssemblyInfo.cs
+++ c#/gameChat/ChatClientform/obj/Debug/net8.0-windows/ChatClientform.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatClientform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb78b79b977c5b6c2f69c5b1cc06fd97a1efd25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78eccd2336d8044907ab7135d65e70b17e1d7e24")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatClientform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatClientform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- c#/gameChat/ServerChat/Program.cs
+++ c#/gameChat/ServerChat/Program.cs
@@ -9,15 +9,10 @@ namespace ServerChat
         static async Task Main(string[] args)
         {
             Console.WriteLine("Chat Server");
-            Console.Write("Enter port number: ");
-            if (!int.TryParse(Console.ReadLine(), out int port))
-            {
-                Console.WriteLine("Invalid port number. Using default port 8888.");
-                port = 8888;
-            }
+          
 
             var server = new ChatServer();
-            await server.StartAsync(port);
+            await server.StartAsync(8888);
         }
     }
 }
